02ec7c858be990f11c485767ee95e0e0d1032d5a
Thibahan Sachchithananthan <s.thibahan@gmail.com>
2022-07-10 16:18:24 +0200

    added function for calculating the image gradient

--- opticalFlow/utils.py
+++ opticalFlow/utils.py
@@ -1,15 +1,18 @@
+from typing import Tuple
 import numpy as np
+from numpy.typing import NDArray
 import cv2
+from scipy.signal import convolve2d
 
 
-def get_image_pyramide(image: np.ndarray, kernel: tuple,
+def get_image_pyramide(image: NDArray, kernel: tuple,
                        scale: float, sigma: float,
                        size: int) -> list:
     """Calculates a image pyramide and returns the images of the
     pyramide as a list.
 
     Args:
-        image (np.ndarray): Image as a numpy array.
+        image (NDArray): Image as a numpy array.
         kernel (tuple): Kernel size for the gaussian blur.
         scale (float): Scale to shrink the images.
         sigma (float): Kernel standard deviation.
@@ -37,3 +40,32 @@ def get_image_pyramide(image: np.ndarray, kernel: tuple,
         image = cv2.resize(image, dimension, interpolation=cv2.INTER_CUBIC)
 
     return image_pyramide
+
+
+def get_gradient(im1: NDArray,
+                 im2: NDArray,
+                 kernelX: NDArray = np.array([[-1, 1], [-1, 1]]) * 0.25,
+                 kernelY: NDArray = np.array([[-1, -1], [1, 1]]) * 0.25,
+                 kernelT: NDArray = np.ones((2, 2)) * 0.25) \
+                 -> Tuple[NDArray, NDArray, NDArray]:
+    """This function calulates the image gradient in x, y and t
+    direction for two images.
+
+    Args:
+        im1 (NDArray): Image one.
+        im2 (NDArray): Image two.
+        kernelX (NDArray, optional): Kernel for image gradient.
+        Defaults to np.array([[-1, 1], [-1, 1]])*0.25.
+        kernelY (NDArray, optional): Kernel for image gradient.
+        Defaults to np.array([[-1, -1], [1, 1]])*0.25.
+        kernelT (NDArray, optional): Kernel for image gradient.
+        Defaults to np.ones((2, 2))*0.25.
+
+    Returns:
+        Tuple[NDArray, NDArray, NDArray]: Image gradients in x, y and t.
+    """
+    fx = convolve2d(im1, kernelX, "same") + convolve2d(im2, kernelX, "same")
+    fy = convolve2d(im1, kernelY, "same") + convolve2d(im2, kernelY, "same")
+
+    ft = convolve2d(im1, kernelT, "same") + convolve2d(im2, -kernelT, "same")
+    return fx, fy, ft
